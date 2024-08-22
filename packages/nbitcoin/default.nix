60aaa1771301ad14fd5682f815e7b74896323872
matthewcroughan <matt@croughan.sh>
2024-08-22 23:32:37 +0100

    nbitcoin: patch challengeBytes to use mutinynet

--- packages/nbitcoin/default.nix
+++ packages/nbitcoin/default.nix
@@ -16,6 +16,12 @@ buildDotnetModule {
     hash = "sha256-X7cqYuk8/Wh2YbP7lEs5Tyhii1NB/zn4oVCeR+yK6PQ=";
   };
 
+  prePatch = ''
+    substituteInPlace NBitcoin/Bitcoin.cs --replace-fail \
+      '512103ad5e0edad18cb1f0fc0d28a3d4f1f3e445640337489abb10404f2d1e086be430210359ef5021964fe22d6f8e05b2463c9540ce96883fe3b278760f048f5189f2e6c452ae' \
+      '512102f7561d208dd9ae99bf497273e16f389bdbd6c4742ddb8e6b216e64fa2928ad8f51ae'
+  '';
+
   dotnetFlags = [
     "--property:TargetFramework=netstandard2.1"
     "--property:TargetFrameworks=netstandard2.1"
